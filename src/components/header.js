37a7d29ecdf9ca957a2bd710b422fd6edac5c22d
felipe <fajreyy.bh@gmail.com>
2020-01-20 18:33:48 -0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <header>
+      <h1>Império Digital</h1>
+      <p>Aprenda a ganhar dinheiro do jeito certo</p>
+      <div>
+        <a href={'https://pay.hotmart.com/E20402684W'}><button type="button">Participe agora!</button></a>
+      </div>
+      <img src="./assets/logo.png" alt='' style={{ maxWidth: 525 }} />
+
+    </header>
+  );
+}
+
+export default Header;
