abf6377af42335e6b0ea07e243ecd9110d0b42a0
felipe <fajreyy.bh@gmail.com>
2020-01-20 18:47:59 -0300

    remove menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import Main from "./components/main";
 import Header from "./components/header";
-import Nav from "./components/nav";
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Nav />
         <Header />
         <Main />
       </div>
